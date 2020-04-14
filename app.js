55fb67537081d33dd34fd61ea716806f3ce060a0
Cornelia1991 <Cornelia1991@users.noreply.github.com>
2020-04-14 14:49:42 -0400

    added onload functionality

--- app.js
+++ app.js
@@ -4,19 +4,56 @@ const spoonacular = new Spoonacular;
 //Init UI class
 const ui = new UI;
 
-//Search Input
-const searchBtn = document.getElementById('searchBtn');
-
-
-//Serach input event listener
-searchBtn.addEventListener('click', (e) =>{
-  const searchInput = document.getElementById('searchInput').value;
-  if (searchInput !== '') {
-    spoonacular.getRecipe(searchInput)
+//On Page load
+window.addEventListener("load", (e)=>{
+  if(searchInput !== '') {
+    //use default on page load
+    spoonacular.getRecipe()
     .then(data => {
+      //show 12 cards
       for(let i = 0; i < 12; i++) {
         ui.showRecipe(data.hits[i]); 
       }
-    })
+    });
   }
 });
+
+
+//Search Input
+const searchInput = document.getElementById('searchInput');
+
+//Serach input event listener
+  searchInput.addEventListener('change', (e) => {
+    //Get Input
+    const userText = e.target.value;
+
+    if (userText !== '') {
+      //Make Http Call
+      spoonacular.getRecipe(userText)
+      .then(data => {
+        if(data.hits[0] === undefined) {
+          // Show Alert
+          ui.showAlert('Recipe not found', 'alert alert-dismissible alert-primary');
+        } else {
+          //clear profile before loading new search
+          ui.clearRecipe();
+          //show 12 recipes
+          for(let i = 0; i < 12; i++) {
+            ui.showRecipe(data.hits[i]); 
+          }
+        }
+      });
+    }
+  });
+
+
+//Loading
+// function work() { /*...*/ }
+
+// if (document.readyState == 'loading') {
+//   // loading yet, wait for the event
+//   document.addEventListener('DOMContentLoaded', work);
+// } else {
+//   // DOM is ready!
+//   work();
+// }
--- ui.js
+++ ui.js
@@ -4,16 +4,16 @@ class UI {
   }
 
   showRecipe(choice) {
-    console.log(choice.recipe.url);
+    //html for each recipe card
     this.recipeCard.innerHTML += `
     <div class="col-md-6 col-lg-4 d-flex align-items-stretch">
     <div class="card bg-light mb-3 mt-3">
       <div class="card-body">
           <div id='foodPic'>
               <img class="card-img-top" src='${choice.recipe.image}' alt="Card image cap" />
-              <h3 class="text-primary">${choice.recipe.label}</h3>
-              <h6 class="">Source: ${choice.recipe.source}</h6><br>
-              <div class="btn-group btn-group-toggle" data-toggle="buttons">
+              <h4 class="text-primary">${choice.recipe.label}</h4>
+              <h6 class=""><b>Source:</b> ${choice.recipe.source}</h6><br>
+              <div class="btn-group btn-group-toggle align-self-end" data-toggle="buttons">
                 <label class="btn btn-info active">
                   <input onclick="window.location.href='${choice.recipe.url}';" type="checkbox" autocomplete="off"> More Info
                 </label>
@@ -30,40 +30,39 @@ class UI {
     `;
   }
 
-  //   //Clear Profile
-  // fshowAlert(message, className){
-  //   //Clear ayny remaining alerts
-  //   this.clearAlert();
-  //   //Create alert
-  //   const alert = document.createElement('div');
-  //   //Add class name and message
-  //   alert.className = className;
-  //   //Add Text
-  //   alert.appendChild(document.createTextNode(message));
-  //   //Get Parent
-  //   const container = document.querySelector('.searchContainer');
-  //   const search = document.querySelector('.search');
-  //   //insert alert
-  //   container.insertBefore(alert, search);
+  //Clear Profile
+  showAlert(message, className){
+    //Clear ayny remaining alerts
+    this.clearAlert();
+    //Create alert
+    const alert = document.createElement('div');
+    //Add class name and message
+    alert.className = className;
+    //Add Text
+    alert.appendChild(document.createTextNode(message));
+    //Get Parent
+    const container = document.querySelector('.searchContainer');
+    const search = document.querySelector('.search');
+    //insert alert
+    container.insertBefore(alert, search);
 
-  //   //timeout after 3 secs
-  //   setTimeout(()=>{
-  //     this.clearAlert();
-  //   }, 3000);
-  // }
+    //timeout after 3 secs
+    setTimeout(()=>{
+      this.clearAlert();
+    }, 3000);
+  }
 
   // //Clear alert message
-  // clearAlert(){
-  //   const currentAlert = document.querySelector('.alert');
+  clearAlert(){
+    const currentAlert = document.querySelector('.alert');
 
-  //   if(currentAlert){
-  //     currentAlert.remove();
-  //   }
-  // }
+    if(currentAlert){
+      currentAlert.remove();
+    }
+  }
 
-  // //Show alert message
-  // clearRecipe(){
-  //   this.profile.innerHTML = '';
-  // }
-
+  //Clear Recipe
+  clearRecipe(){
+    this.recipeCard.innerHTML = '';
+  }
 }//END CLASS
